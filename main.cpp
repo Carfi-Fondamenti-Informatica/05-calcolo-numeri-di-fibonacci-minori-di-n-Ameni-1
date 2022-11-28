cd9f8e2d387d6c96603a061bff4ef4bc0af788d0
Ameni-1 <115472533+Ameni-1@users.noreply.github.com>
2022-11-28 09:04:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 int main() {
-   int n=0 , a=1 ,b=1 ,c=0;
+   int n=0 , a=1 ,b=1 , c=0;
    cin >> n;
-   Cout << a << endl << b << endl;
-   for ( int i = 0; i <= n; i++){[
+   cout << a << endl << b << endl;
+   for ( int i = 0; i <= n; i++){
       c = a + b;
       a = b;
       b = c
